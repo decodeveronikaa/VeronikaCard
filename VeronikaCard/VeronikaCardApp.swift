86a9f4bd396778266b96e4f671d4c9b179fb21e9
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-22 14:23:50 -0400

    first commit

---
+++ VeronikaCard/VeronikaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeronikaCardApp.swift
+//  VeronikaCard
+//
+//  Created by Veronica Markova on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct VeronikaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
