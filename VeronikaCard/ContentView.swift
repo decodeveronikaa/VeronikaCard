1232235374a7931d5fc0d920d40b847a68070d5c
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-22 16:16:57 -0400

    init UI commit

--- VeronikaCard/ContentView.swift
+++ VeronikaCard/ContentView.swift
@@ -12,16 +12,44 @@ struct ContentView: View {
         ZStack{
             Color(red: 0.97, green: 0.76, blue: 0.57, opacity: 1.00)
                 .edgesIgnoringSafeArea(.all)
-        Text("Veronika Markova")
-            .font(Font.custom("Pacifico-Regular", fixedSize: 40))
-            .bold()
-            .foregroundColor(.white)
+            VStack {
+                
+                Image("Veronika")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 3))
+                Text("Veronika Markova")
+                    .font(Font.custom("Pacifico-Regular", fixedSize: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("1/2 iOS developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                Divider()
+                HStack {
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .frame(height: 30)
+                        .overlay(HStack {
+                            Image(systemName: "phone.fill")
+                                .foregroundColor(.green)
+                            Text("+971586000000")
+                            
+                        })
+                        .padding(.all)
+                }
+                
+                
+            }
             
+        }
     }
-}
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11 Pro")
     }
 }
